33edb51e1aa5a4495a49607aa2fca1349ba7cd02
Katcha <katcha@Nicos-MacBook-Pro.local>
2022-05-20 19:22:59 +0300

    major: basically finished front.py

--- ttt_web/ttt_app.py
+++ ttt_web/ttt_app.py
@@ -22,6 +22,28 @@ def create_app(db):
         else:
             return json.dumps({"message":"Request Fields:\nboard_size\nplayer1\nplayer2\ngame_name"})
 
+    @ttt_app.route('/currentplayer', methods =['POST'])
+    def current_player():
+    #who's move is it, x/o?
+        game_id = request.get_json()['game_id']
+        conn, cur=ttt.db_connect(db)
+        sql = "SELECT COUNT(*) FROM move_log WHERE game_id = %s AND player_symbol = %s"
+        cur.execute(sql,(game_id,"x"))
+        x_count= cur.fetchone()[0]
+        cur.execute(sql,(game_id,"o"))
+        o_count = cur.fetchone()[0]
+        player_symbol = "x"
+        if x_count>o_count:
+            player_symbol = "o" 
+        cur.execute("SELECT player1, player2 FROM game_log WHERE game_id = %s",(game_id,))
+        players = cur.fetchone()
+        if player_symbol == "x":
+            player = players[0] #player1
+        else:
+            player = players[1] #player2
+        cur.close()
+        conn.close()
+        return json.dumps({"current_player":player})
 
     @ttt_app.route('/move', methods=['GET','POST'])
     def make_move():
@@ -32,7 +54,7 @@ def create_app(db):
             move:str = move_POST_input["move"]
             #check if move is in correct format 
             if not ttt.check_convertable(move):
-                return json.dumps({"message":'Move must be a letter followed by a number within the range of the board, eg A2'})
+                return json.dumps({"message":'Move must be a letter followed by a number within the range of the board, eg A2', "success": False, "game_over":False})
             # convert move (eg. A1) to coordinates (0,0)
             coordinates:list = ttt.convert(move)
             #open db connection
@@ -41,7 +63,7 @@ def create_app(db):
                 #is move valid?
                 valid:Tuple[bool,str] = ttt.check_valid(game_id, coordinates, cur)
                 if not valid[0]:
-                    return json.dumps({"message":valid[1]})
+                    return json.dumps({"message":valid[1], "success": False, "game_over":False}) #why not valid
                 #add move to db
                 player_symbol = ttt.update_move_log(game_id, coordinates,conn,cur)
                 
@@ -59,13 +81,13 @@ def create_app(db):
                 conn.close()
             if win[0]:
                 #TODO: change to player name
-                return json.dumps({"message":f"{player_symbol} wins! Game Over."})
+                return json.dumps({"message":f"{player_symbol} wins! Game Over.", "success":True, "game_over":True})
             elif stale_mate:
-                return json.dumps({"message":"Stalemate! Game Over"})
+                return json.dumps({"message":"Stalemate! Game Over", "success":True, "game_over":True})
             else:
-                return json.dumps({"message" : "move successful"})
+                return json.dumps({"message":"move added","success":True,"game_over":False})
         else:
-            return json.dumps({"message": "Request Fields: game_id, move"})
+            return json.dumps({"message": "Request Fields: game_id, move", "success":False,"game_over":False})
 
     @ttt_app.route("/games", methods=['GET','POST'])
     def display_games():
@@ -76,21 +98,24 @@ def create_app(db):
                 display_POST_input = request.get_json()
                 name_search = display_POST_input['name']
                 sql = """SELECT game_name, game_id FROM game_log 
-                            WHERE game_name LIKE %s ORDER BY game_name"""
+                            WHERE winner is null AND game_name LIKE %s ORDER BY game_name"""
                 str_subs = (f'%{name_search}%',)
                 cur.execute(sql,str_subs)
                 game_names = cur.fetchall()
                 count = len(game_names)
             else:
             # get all game_id and game_name 
-                cur.execute("SELECT game_name, game_id FROM game_log")
+                cur.execute("SELECT game_name, game_id FROM game_log WHERE winner is null")
                 game_names = cur.fetchall()
                 count = len(game_names)
         finally:
             cur.close()
             conn.close()
-        # TODO: convert message to dict id:name - VISUAL
-        return json.dumps({"message": f"Game Name | Game ID \n{game_names}", "result_count":count})
+        # add games to games_dict 
+        games_dict = {"Game Name":"Game ID"}
+        for i in range(count):
+            games_dict[game_names[i][0]] = game_names[i][1]
+        return json.dumps({"message": games_dict, "count":count})
 
     @ttt_app.route("/users", methods=['GET'])
     def display_users():
@@ -102,19 +127,19 @@ def create_app(db):
             conn.close()
         return json.dumps({"message":f'{users}'})
 
-    @ttt_app.route("/userstats", methods=['GET','POST'])
-    def display_userstats():
+    @ttt_app.route("/userstats", methods=['GET'])
+    def return_userstats():
         try: 
             conn, cur = ttt.db_connect(db)
             leader_board:dict = ttt.generate_leader_board(conn,cur)
         finally:
             cur.close()
             conn.close()
-        if request.method == 'POST':
-            user = request.get_json()['user_name']
-            return json.dumps({"message":f'{user}, {leader_board[user]}'})
-        else:  
-            return json.dumps({"message":f'{leader_board}'})
+        # if request.method == 'POST':
+        #     user = request.get_json()['user_name']
+        #     return json.dumps({"message":f'{user}, {leader_board[user]}'})
+        # else:  
+        return json.dumps(leader_board)
 
     @ttt_app.route("/viewgame", methods=["GET","POST"])
     def view_game():
@@ -130,6 +155,16 @@ def create_app(db):
             cur.close()
             conn.close()
         return json.dumps({"message":gb})
+    
+    @ttt_app.route("/game", methods = ['POST'])
+    def game_info():
+        game_id = request.get_json()['game_id']
+        conn, cur = ttt.db_connect(db)
+        cur.execute("SELECT game_name, game_id, player1, player2 FROM game_log WHERE game_id = %s",(game_id,))
+        data = cur.fetchone()
+        game_info = {"game_name":data[0], "game_id":data[1],"player1":data[2],"player2":data[3]}
+        return json.dumps(game_info)
+
     return ttt_app
 
 if __name__ == "__main__":
--- ttt_web/ttt_front.py
+++ ttt_web/ttt_front.py
@@ -5,10 +5,20 @@ import pdb
 from typing import Dict, List, Tuple, Union
 from ttt_web.ttt_app import create_app
 
-EXIT = 'exit'
-# app = create_app('play_game')
+INTRO_TEXT = """
+Welcome to Tick Tack Toe
 
-def create_new_game()-> int:
+What you can do:
+* create a new game with a name, player user names and selected board size (3x3 or 4x4)
+* Continue a saved game
+* Make moves on your turn using coordinates eg. A2
+* Type "userstats" on your turn to access user statistics - view the leader board or view the stats for a particular player
+* Type 'exit' to quit the game - your game will automatically be saved
+
+ENJOY!
+"""
+
+def create_new_game()-> dict:
     game_name = input("Game Name: ")
     board_size = input("Select board size: A. 3x3 B. 4x4 \n>")
     if board_size.upper() == 'B' or board_size[0] == '4':
@@ -22,10 +32,10 @@ def create_new_game()-> int:
     # success
     if flask_game_created['message'] == 'game successfully created':
         game_id = flask_game_created["game_id"]
-        return game_id
+        return {"game_id":game_id, "player1":player1, "player2":player2}
     else:
         print(flask_game_created['message'])
-        return 0
+        return {"game_id":0, "player1":player1, "player2":player2}
 
 # VISUAL | Adds row and colum labels - asethetic only
 def add_axis_title(gb:List[List]) -> List[List]:
@@ -56,127 +66,107 @@ def print_beautiful_board(gb_copy:List[List]) -> str:
     print(board_string)
     return board_string
 
+def display_board(game_id:int)->str:
+    flask_viewgame = requests.post("http://localhost:5001/viewgame", json={'game_id':game_id})
+    gb = flask_viewgame.json()['message']
+    print_beautiful_board(add_axis_title(gb))
+
+def load_game()->dict:
+    name_search = input("search for a game or press enter to list all games: ")
+    if name_search == "": # all
+        flask_games = requests.get("http://localhost:5001/games").json()
+        for key in flask_games['message']:
+            print(key+" : " + str(flask_games['message'][key]))
+        # print(flask_games["message"]) #dict of all games
+        # TODO: what if they don't put in correct input? 
+        game_id = int(input("Select the game_id for the game you want to continue: "))    
+    else: # subset
+        flask_games = requests.post("http://localhost:5001/games", json={'name':name_search}).json()
+        if flask_games['count'] == 1: # unique
+            game_board = flask_games['message']['name_search']
+        else: # selection needed
+            print(flask_games['message']) 
+            game_id = int(input("Select the game_id for the game you want to continue: ")) 
+            # TODO: if bad input ...
+    # display game info
+    flask_game = requests.post("http://localhost:5001/game", json={"game_id":game_id}).json()
+    print("\nGAME INFO")
+    print("Game Name: "+flask_game['game_name'])
+    print("Game ID: "+str(flask_game["game_id"]))
+    print("Player1: "+flask_game['player1'])
+    print("Player2: "+flask_game['player2'])
+    # determin current player
+    flask_current_player = requests.post("http://localhost:5001/currentplayer", json={"game_id":game_id}).json()
+    player = flask_current_player['current_player']
+    return {"game_id":game_id, "player": player, "player1": flask_game['player1'],"player2": flask_game['player2']}
+
+def make_move(game_id:int, player:str)->dict:
+    move = input(f"{player} Move: ")
+    # special cases
+    if move == "exit":
+        return {'message':'Game Exited', "game_over":True}
+    if move == "userstats":
+        return {'message': 'userstats', "success":False, 'game_over':False}
+    # TODO add more: leader board, undo
+    move_dict = {'move': move, "game_id":game_id}
+    flask_move = requests.post("http://localhost:5001/move", json=move_dict).json()
+    return flask_move
+
+def display_userstats():
+    select_user = input("Type a username to view their stats or press enter to view the full leader board: ")
+    flask_leaderboard = requests.get('http://localhost:5001/userstats').json()
+    if select_user == "":
+        for key in flask_leaderboard:
+            print(key, flask_leaderboard[key])
+    else:
+        print("\n")
+        print(flask_leaderboard['Player'])
+        print(flask_leaderboard[f'{select_user}'])
+
+
 def run():
-    print("Intro Text")
+    # Intro and Instructions
+    print(INTRO_TEXT)
+    # Get game type
     resp_new = input("\nWhat kind of game do you want? \nA. New Game \nB. Saved Game\n> ")
     # new
     if resp_new[0].capitalize() == 'A' or resp_new[0].capitalize() == 'N':
-        game_id = create_new_game()
-        # game creation failed
-        if game_id == 0:
-            while game_id == 0:
-                game_id = create_new_game()
+        game_info = create_new_game()
+        game_id = game_info["game_id"]
+        player1 = game_info["player1"]
+        player2 = game_info["player2"]
+        player = player1
+        # TODO: what if game creation failed: if id==0, while id==0
         # game creation succeeded, get empty_board
-        flask_viewgame = requests.post("http://localhost:5001/viewgame", json={'game_id':game_id})
-        gb = flask_viewgame.json()['message']
-        print_beautiful_board(add_axis_title(gb))
-    else: #load game
-        name_search = input("search for a game or press enter to list all games: ")
-        if name_search == "": # all
-            flask_games = requests.get("http://localhost:5001/games").json()
-            print(flask_games["message"])
-            # TODO: what if they don't put in correct input? 
-            game_id = int(input("Select the game_id for the game you want to continue: "))    
-        else:
-            flask_games = requests.post("http://localhost:5001/games", json={'name':name_search}).json()
-            if flask_games['count'] == 1:
-                #load game directly
-                pass
-            else:
-                print(flask_games['message'])
-                game_id = int(input("Select the game_id for the game you want to continue: ")) 
-                # TODO: if bad input ...
-            
-
-        
-
-
-
-
-
-
-            
-        
-
-    #new
-    return "DONE RUN"
-    
-
-def play() -> str:
-    # DB connection has to happen in backend - removed from here
-    
-    print("INTRO TEXT") #fine, need to change to suit new game
-
-    resp_game_type = input("\nWhat kind of game do you want? \nA. New Game \nB. Saved Game\n> ")
-    # Saved game
-    if resp_game_type[0].capitalize() == 'B' or resp_game_type[0].capitalize() == 'S':
-
-        flask_resp = requests.post("http://localhost:5001", data={})
-        game_state = load_saved_board(cur)
-        gb = game_state.gb
-        
-    #New game
+        display_board(game_id)    
+    # old
     else:
-        player1, player2 = select_users(cur) 
-        chosen_board_size = choose_board_size()
-        if chosen_board_size == 4:
-            gb = copy.deepcopy(EMPTY_4X4_BOARD)
-        elif chosen_board_size == 3: 
-            gb = copy.deepcopy(EMPTY_3X3_BOARD)
-        game_state = GameState('new',gb,'x',list(),player1,player2)
-    print_beautiful_board(add_axis_title(game_state.gb))
-
-    winner = False
-    while not winner:
-        processed_user_input = get_move(game_state)
-        # Save game
-        if processed_user_input == 'save':
-            save_game(game_state,cur, conn)
-            print('your game has been saved')
-            return EXIT
-        # Undo last move
-        elif processed_user_input == 'undo':
-            last_coordinate = game_state.move_log.pop()
-            game_state.gb = undo_turn(game_state.gb,last_coordinate)
-            print_beautiful_board(add_axis_title(game_state.gb))
-        # Print leader board and continue
-        elif processed_user_input == 'leader_board':
-            print("\nLEADER BOARD")
-            display_leader_board(cur)
-            if game_state.current_player == 'x':
-                game_state.current_player = 'o'
-            elif game_state.current_player == 'o':
-                game_state.current_player = 'x'
-        # End game - resign 
-        elif processed_user_input == 'end':
-            print('Game Resigned')
-            return EXIT 
-    # update game with new move
-        else:
-            game_state.gb = update_board(game_state, processed_user_input)
-            game_state.move_log.append(processed_user_input)
-            winner = check_win(game_state.gb, game_state.player1, game_state.player2)
-            # Someone WON
-            if winner:
-                loser = game_state.player1
-                if winner == game_state.player1:
-                    loser = game_state.player2
-                update_log(game_state,cur,conn,winner,loser)
-                # update_user_stats(winner,loser)  
-                break 
-            # STALE MATE
-            if len(game_state.move_log) == len(game_state.gb)**2:
-                print('STALE MATE, GAME OVER!')
-                update_log(game_state,cur,conn)
-                return EXIT 
-        if game_state.current_player == 'x':
-            game_state.current_player = 'o'
-        elif game_state.current_player == 'o':
-            game_state.current_player = 'x'
-            
-    return EXIT
+        game_info = load_game()
+        game_id = game_info['game_id']
+        player1 = game_info['player1']
+        player2 = game_info['player2'] ###not working!!
+        player = game_info['player']
+        display_board(game_id)
+    # Make move
+    move_outcome = make_move(game_id, player)
+    while not move_outcome['game_over']: # game not over
+            if not move_outcome['success']: 
+                if move_outcome['message'] == 'userstats': #special input
+                    display_userstats()
+                else: # move failed   
+                    print(move_outcome['message'])
+                    print('try again')
+            else: 
+                if player == player1: #update current player
+                    player = player2
+                else: 
+                    player = player1
+            display_board(game_id) # display updated board
+            move_outcome = make_move(game_id, player) # next move
+    display_board(game_id) 
+    print(move_outcome['message']) 
+    return
    
-
 if __name__ == "__main__":
     run()
 
